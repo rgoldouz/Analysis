f9ec4eaf9e743cc0208722f1cc393c11e112185e
Jack Li <li.jingya@husky.neu.edu>
2020-05-28 15:16:29 +0800

    Update hadd.py

--- hists/hadd.py
+++ hists/hadd.py
@@ -5,18 +5,23 @@ import readline
 import string
 
 
-sys.path.append('/user/rgoldouz/NewAnalysis2020/Analysis/bin')  
+sys.path.append(os.path.join(os.path.dirname(sys.path[0]),'bin'))
 
 import Files_2016
 import Files_2017
 import Files_2018
+import Files_2017_A
+
 SAMPLES = {}
-mc_2016 = True
-data_2016 = True
-mc_2017 = True
-data_2017 = True
-mc_2018 = True
-data_2018 = True
+mc_2016 = False
+data_2016 = False
+mc_2017 = False
+data_2017 = False
+mc_2018 = False
+data_2018 = False
+
+SAMPLES.update(Files_2017_A.mc2017_samples)
+SAMPLES.update(Files_2017_A.data2017_samples)
 
 if mc_2016:
     SAMPLES.update(Files_2016.mc2016_samples)
@@ -32,18 +37,30 @@ if data_2018:
     SAMPLES.update(Files_2018.data2018_samples)
 
 
-addedFilesData = {"2016": [], "2017": [], "2018": []} 
-addedFilesMc = {"2016": [], "2017": [], "2018": []}
+addedFilesData = {"2017": []}
+addedFilesMc = {"2017": []}
+addedFilesTTV = {"2017": []}
+addedFilesVV = {"2017": []}
+addedFilesTTbar = {"2017": []}
 
 for key, value in SAMPLES.items():
     year = value[3]
-    os.system('rm '+ year + '/' +key + '.root')
+    os.system('rm '+ key + '.root')
     nf = 40
     if value[1]=='data':
         addedFilesData[year].append( year + '/' + key + '.root ')
-    if ('TTTo2L2Nu' not in key) and ('DY' not in  key) and ('ST' not in  key) and ('WJetsToLNu' not in  key) and (value[1]=='mc'):
+    elif ('TTW' in key) or ('TTZ' in  key):
+        addedFilesTTV[year].append(  year + '/' + key + '.root ')
+    elif ('WW' in key) or ('WZ' in  key) or ('ZZ' in  key):
+        addedFilesVV[year].append(  year + '/' + key + '.root ')
+    elif ('TTTo' in key):
+        addedFilesTTbar[year].append(  year + '/' + key + '.root ')
+    else:
         addedFilesMc[year].append(  year + '/' + key + '.root ')
-    hadd='hadd ' + year + '/' + key + '.root '
+    if ('SMEFTfr' in key):
+        hadd='hadd ' + key + '.root '
+    else:
+        hadd='hadd ' + year + '/' + key + '.root '
     for idx, S in enumerate(value[0]):
         if value[1]=='data':
             nf = 255
@@ -56,63 +73,72 @@ for key, value in SAMPLES.items():
 
 os.system('rm *_data.root')
 os.system('rm *_others.root')
-hadddata_2016 ='hadd 2016_data' + '.root ' + ' '.join(addedFilesData['2016'])
+os.system('rm *_TTV.root')
+os.system('rm *_VV.root')
+os.system('rm *_TTbar.root')
+#hadddata_2016 ='hadd 2016_data' + '.root ' + ' '.join(addedFilesData['2016'])
 hadddata_2017 ='hadd 2017_data' + '.root ' + ' '.join(addedFilesData['2017'])
-hadddata_2018 ='hadd 2018_data' + '.root ' + ' '.join(addedFilesData['2018'])
+#hadddata_2018 ='hadd 2018_data' + '.root ' + ' '.join(addedFilesData['2018'])
 
-haddmc_2016 ='hadd 2016_others' + '.root ' + ' '.join(addedFilesMc['2016'])
+#haddmc_2016 ='hadd 2016_others' + '.root ' + ' '.join(addedFilesMc['2016'])
 haddmc_2017 ='hadd 2017_others' + '.root ' + ' '.join(addedFilesMc['2017'])
-haddmc_2018 ='hadd 2018_others' + '.root ' + ' '.join(addedFilesMc['2018'])
+#haddmc_2018 ='hadd 2018_others' + '.root ' + ' '.join(addedFilesMc['2018'])
+haddTTV_2017 ='hadd 2017_TTV' + '.root ' + ' '.join(addedFilesTTV['2017'])
+haddVV_2017 ='hadd 2017_VV' + '.root ' + ' '.join(addedFilesVV['2017'])
+haddTTbar_2017 ='hadd 2017_TTbar' + '.root ' + ' '.join(addedFilesTTbar['2017'])
 
-os.system(haddmc_2016)
+#os.system(haddmc_2016)
 os.system(haddmc_2017)
-os.system(haddmc_2018)
+#os.system(haddmc_2018)
 
-os.system(hadddata_2016)
+#os.system(hadddata_2016)
 os.system(hadddata_2017)
-os.system(hadddata_2018)
+#os.system(hadddata_2018)
+os.system(haddTTV_2017)
+os.system(haddVV_2017)
+os.system(haddTTbar_2017)
 
-os.system('rm *_DY.root')
-os.system('hadd 2016_DY.root 2016/2016_DYM50.root 2016/2016_DYM10to50.root')
-os.system('hadd 2017_DY.root 2017/2017_DYM50.root 2017/2017_DYM10to50.root')
-os.system('hadd 2018_DY.root 2018/2018_DYM50.root 2018/2018_DYM10to50.root')
+#os.system('rm *_DY.root')
+#os.system('hadd 2016_DY.root 2016/2016_DYM50.root 2016/2016_DYM10to50.root')
+#os.system('hadd 2017_DY.root 2017/2017_DYM50.root 2017/2017_DYM10to50.root')
+#os.system('hadd 2018_DY.root 2018/2018_DYM50.root 2018/2018_DYM10to50.root')
 
-os.system('rm *tW.root')
-os.system('hadd 2016_ST_tW.root 2016/2016_ST_tW.root 2016/2016_ST_atW.root')
-os.system('hadd 2017_ST_tW.root 2017/2017_ST_tW.root 2017/2017_ST_atW.root')
-os.system('hadd 2018_ST_tW.root 2018/2018_ST_tW.root 2018/2018_ST_atW.root')
+#os.system('rm *tW.root')
+#os.system('hadd 2016_ST_tW.root 2016/2016_ST_tW.root 2016/2016_ST_atW.root')
+#os.system('hadd 2017_ST_tW.root 2017/2017_ST_tW.root 2017/2017_ST_atW.root')
+#os.system('hadd 2018_ST_tW.root 2018/2018_ST_tW.root 2018/2018_ST_atW.root')
 
-os.system('rm *_LFV*')
-os.system('hadd 2016_LFVVecC.root 2017/2016_LFVTtVecC.root 2017/2016_LFVStVecC.root')
-os.system('hadd 2017_LFVVecC.root 2017/2017_LFVTtVecC.root 2017/2017_LFVStVecC.root')
-os.system('hadd 2018_LFVVecC.root 2017/2018_LFVTtVecC.root 2017/2018_LFVStVecC.root')
+#os.system('rm *_LFV*')
+#os.system('hadd 2016_LFVVecC.root 2017/2016_LFVTtVecC.root 2017/2016_LFVStVecC.root')
+#os.system('hadd 2017_LFVVecC.root 2017/2017_LFVTtVecC.root 2017/2017_LFVStVecC.root')
+#os.system('hadd 2018_LFVVecC.root 2017/2018_LFVTtVecC.root 2017/2018_LFVStVecC.root')
+#
+#os.system('hadd 2016_LFVVecU.root 2017/2016_LFVTtVecU.root 2017/2016_LFVStVecU.root')
+#os.system('hadd 2017_LFVVecU.root 2017/2017_LFVTtVecU.root 2017/2017_LFVStVecU.root')
+#os.system('hadd 2018_LFVVecU.root 2017/2018_LFVTtVecU.root 2017/2018_LFVStVecU.root')
 
-os.system('hadd 2016_LFVVecU.root 2017/2016_LFVTtVecU.root 2017/2016_LFVStVecU.root')
-os.system('hadd 2017_LFVVecU.root 2017/2017_LFVTtVecU.root 2017/2017_LFVStVecU.root')
-os.system('hadd 2018_LFVVecU.root 2017/2018_LFVTtVecU.root 2017/2018_LFVStVecU.root')
 
-
-Y = ['2016','2017','2018']
-Sam = ['TTTo2L2Nu','WJetsToLNu']
-
-for y in Y:
-    for s in Sam:
-        txt = 'cp ' + y + '/' + y + '_' + s + '.root .'
-        os.system(txt)
-
-allSamples = ['TTTo2L2Nu','ST_tW','WJetsToLNu','data', 'others', 'DY', 'LFVVecU', 'LFVVecC']
-allMCSamples = ['TTTo2L2Nu','ST_tW','WJetsToLNu', 'others', 'DY']
-
-for s in allSamples:
-    os.system('rm All_' + s + '.root ')
-    haddall='hadd All_' + s + '.root '
-    for y in Y:
-        haddall +=  y + '_' + s + '.root '
-    os.system(haddall) 
-      
-for y in Y:
-    os.system('rm mc_' + y + '.root ') 
-    haddall='hadd mc_' + y + '.root '
-    for s in allMCSamples:
-        haddall +=  y + '_' + s + '.root '
-    os.system(haddall)
+#Y = ['2017']
+#Sam = ['TTTo2L2Nu','WJetsToLNu']
+#
+#for y in Y:
+#    for s in Sam:
+#        txt = 'cp ' + y + '/' + y + '_' + s + '.root .'
+#        os.system(txt)
+#
+#allSamples = ['TTTo2L2Nu','ST_tW','WJetsToLNu','data', 'others', 'DY', 'SMEFTfr_ST_vector_emutc', 'SMEFTfr_ST_vector_emutu', 'SMEFTfr_TT_vector_emutc', 'SMEFTfr_TT_vector_emutu', 'SMEFTfr_ST_clequ1_emutc', 'SMEFTfr_ST_clequ3_emutu', 'SMEFTfr_ST_clequ3_emutc', 'SMEFTfr_TT_clequ1_emutc', 'SMEFTfr_TT_clequ1_emutu', 'SMEFTfr_TT_clequ3_emutu']
+#allMCSamples = ['TTTo2L2Nu','ST_tW','WJetsToLNu', 'others', 'DY']
+#
+#for s in allSamples:
+#    os.system('rm All_' + s + '.root ')
+#    haddall='hadd All_' + s + '.root '
+#    for y in Y:
+#        haddall +=  y + '_' + s + '.root '
+#    os.system(haddall)
+#
+#for y in Y:
+#    os.system('rm mc_' + y + '.root ')
+#    haddall='hadd mc_' + y + '.root '
+#    for s in allMCSamples:
+#        haddall +=  y + '_' + s + '.root '
+#    os.system(haddall)
