fdb53d15edf79ccd8a3e84668b966b7d42ddcef7
Ashley <ash.marie.parker@gmail.com>
2020-09-13 23:02:09 +0200

    Add DY samples from TOP NANOAOD to test the new code on

---
+++ bin/nano_files_2017.py
@@ -0,0 +1,11 @@
+import sys
+import os
+import subprocess
+import readline
+import string
+
+data2017_samples = {}
+mc2017_samples = {}
+#data2017_samples ['DYM10to50'] = ['address', 'data/mc','dataset','year', 'run', 'cross section','lumi','Neventsraw']                        
+
+mc2017_samples['2017_DYM10to50'] = [['/store/user/piedavid/topNanoAOD/v6-1-1/2017/DYJetsToLL_M-10to50_TuneCP5_13TeV-madgraphMLM-pythia8/TopNanoAODv6-1-1_2017/200613_065556/0000/'], 'mc','','2017', '','18610','41.53','39521230']
