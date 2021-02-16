e409a66d14a6c2889a8082cf6ae1b3fbd412740f
Jack Li <li.jingya@husky.neu.edu>
2021-02-16 21:44:23 +0100

    Changes to debug statement

--- src/MyAnalysis.cc
+++ src/MyAnalysis.cc
@@ -442,21 +442,19 @@ void MyAnalysis::Loop(TString fname, TString data, TString dataset ,TString year
   if (fChain == 0) return;
   //Long64_t nentries = fChain->GetEntriesFast();
   Long64_t nbytes = 0, nb = 0;
-  Long64_t ntr = fChain->GetEntries ();
+  Long64_t ntr = eList->GetN();
   // loop over number of entries
 
   
 // test on just 100 events ??? fix this!
   // Long64_t
-  for ( Long64_t  kentry=0; kentry<eList->GetN();kentry++) {
+  for ( Long64_t  kentry=0; kentry<ntr;kentry++) {
     //for (Long64_t jentry=0; jentry<100;jentry++) {
     Long64_t   jentry = eList->Next();
     Long64_t   ientry = LoadTree(jentry);
     if (ientry < 0) break;
     nb = fChain->GetEntry(jentry);   nbytes += nb;
-    displayProgress(jentry, ntr) ;
-
-
+    displayProgress(kentry, ntr) ;
 
     triggerPassEE = false;
     triggerPassEMu = false;
@@ -623,16 +621,17 @@ void MyAnalysis::Loop(TString fname, TString data, TString dataset ,TString year
     //cout<< "triggerPassEE" << " " << "triggerPassEMu" << "  " << "triggerPassMuMu" <<endl;
 
 
-
-    //
-    //}
-   // if(!(triggerPassEE || triggerPassEMu || triggerPassMuMu)) continue;
+    if (verbose ) {
+    cout << ".............................................................................................." << endl;
+    cout << "event " << event << endl;
+    }
+    if(!(triggerPassEE || triggerPassEMu || triggerPassMuMu)) continue;
     //if (verbose ) {
     //    cout<<" event passed triggers!  "<<endl;
     // }
-   // if(!metFilterPass) continue;
+    if(!metFilterPass) continue;
     if (verbose ) {
-        cout<<" event passed MET filters!  "<<endl;
+        cout<<" event passed Trigger & MET filters! "<<endl;
     };
 // lepton selection
   selectedLeptons = new std::vector<lepton_candidate*>();//typlical ordered by pT
@@ -641,8 +640,6 @@ void MyAnalysis::Loop(TString fname, TString data, TString dataset ,TString year
   Ms=0;
 
      if (verbose ){
-     cout << ".............................................................................................." << endl;
-    cout << "event " << jentry << endl;   
       cout << "There are  " << nElectron << " Electrons and " << nMuon  << " Muons as well as  " <<  nJet << " Jets "<< endl ;   
       //cout << "mass of Jet 0   " << Jet_mass[0] << " phi of electron 0    " << Electron_phi[0] << endl ;   
      cout << "Electron loop begins"  << endl;   
